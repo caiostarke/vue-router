6f68ca31c2dd7764e0c0732c061d5e5849edc746
crowlke <caiostarke@hotmail.com>
2024-03-08 12:49:12 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/" class="link">Home</RouterLink>
+        <RouterLink to="/users">Users</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+  .link {
+    margin-right: 10px;
+  }
+</style>
